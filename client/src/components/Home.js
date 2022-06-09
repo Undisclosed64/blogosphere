062e342bde64654de7e6f794a73354e5c9369afe
Undisclosed <undisclosed674@gmail.com>
2022-06-09 11:42:41 -0700

    style home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,8 @@ import '../App.css';
 import { useNavigate } from "react-router-dom";
 import {FaUserCircle} from 'react-icons/fa';
 
-
+const date = new Date();
+console.log(date.toDateString())
 function Home(props) {
     const stories = props.post.stories;
    const navigate = useNavigate();
@@ -17,30 +18,35 @@ function Home(props) {
 
             <div className='homePage'>
             <h1 id='blog-name'>Techno Diary</h1>
-            <p className='blog-intro'>Where right guidance find you!</p>
+            <p className='blog-intro'>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
             </div>
 
+<div className='section-story'>
         <div className='story-container'>
             {stories.map(story => 
             <div key={story._id}className="story-wrapper"onClick={()=>storyModelNavigate(story._id)}>
                 
             <img src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG5hdHVyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=600&q=60"alt=""className='blogImg'></img>
             <div className='story-info'>
+
+            <div className='publish-info'>
+
             <div className='author-profile'>
              <FaUserCircle className='author-icon'/>
             <span>{story.author.username}</span>
             </div>
-            <span className='seperator'>
-               |
-           </span>
-            <div className='written-date'>
-                 {new Date(story.dated).toDateString()}
-            </div>  
+
+            <span className='seperator'>|</span>
+            <div className='written-date'>{new Date(story.dated).toDateString()}</div> 
+            </div> 
+            
             <h2 className='story-title'>{story.title}</h2>
             <p className='story-desc'>{story.text}</p>
              </div>
              </div>
             )}
+        </div>
         </div>
         </section>
     );
