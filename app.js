eb4f4e5224051196115f0bd4affc215580c1039e
Undisclosed <undisclosed674@gmail.com>
2022-06-13 05:22:43 -0700

    remove server.js additions

--- app.js
+++ app.js
@@ -49,10 +49,6 @@ app.use('/api/stories',commentRouter);
 
 if(process.env.NODE_ENV === 'production'){
 app.use(express.static('client/build'));
-
-app.get('*',(req,res)=> {
-  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-})
 }
 
 app.listen(PORT, () => {
