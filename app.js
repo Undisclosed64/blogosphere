f1a4db033f35af09a7570c29db55a35d96785052
Undisclosed <undisclosed674@gmail.com>
2023-01-27 20:03:31 -0800

    add changes

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
-const cors = require('cors');
-const multer = require('multer');
-const authRouter = require('./routes/auth');
-const storyRouter = require('./routes/stories');
-const commentRouter = require('./routes/comments');
-
-
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const cors = require("cors");
+const multer = require("multer");
+const authRouter = require("./routes/auth");
+const storyRouter = require("./routes/stories");
+const commentRouter = require("./routes/comments");
 
 //Set up mongoose connection
 const mongoose = require("mongoose");
 const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 var app = express();
 app.use(cors());
@@ -22,42 +20,37 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const PORT = 4000 || process.env.PORT;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const storage = multer.diskStorage({
-  destination:(req,file,cb) =>{
-    cb(null,"images")
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-  },filename:(req,file,cb) => {
-    cb(null,req.body.name)
-  }
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-const upload = multer({storage:storage});
-app.post("/api/upload",upload.single("file"),(req,res) => {
-  res.status(200).json("File has been uploaded")
-})
+app.use("/api/auth", authRouter);
+app.use("/api/stories", storyRouter);
+app.use("/api/stories", commentRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.use('/api/auth',authRouter);
-app.use('/api/stories',storyRouter);
-app.use('/api/stories',commentRouter);
-
-
-if(process.env.NODE_ENV === 'production'){
-app.use(express.static('client/build'));
-
-app.get('*',(req,res)=> {
-  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
-
+  console.log(`Server listening on port ${PORT}`);
+});
 
 module.exports = app;
