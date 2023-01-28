33a976c68fb3107b1e96b8e8e0a6706335ca07c8
Undisclosed <undisclosed674@gmail.com>
2023-01-27 20:39:01 -0800

    change port spelling

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
